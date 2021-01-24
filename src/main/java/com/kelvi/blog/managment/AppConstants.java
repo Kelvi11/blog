20a71066f78201fc2a5a3f6e5443ab0c5666e383
Kelvin <kelvindervishllari@gmail.com>
2021-01-24 17:37:03 +0100

    infrastucture

---
+++ src/main/java/com/kelvi/blog/managment/AppConstants.java
@@ -0,0 +1,8 @@
+package com.kelvi.blog.managment;
+
+public class AppConstants {
+
+    //Endpoint paths
+    public static final String API_PATH = "/api/v1";
+    public static final String COMPANIES_PATH = API_PATH + "/companies";
+}
